b21437221daf636cbdf0f8dbe75450714641aa4d
Joseph-kdev <cjkinuthia5@gmail.com>
2025-11-29 17:46:51 +0300

    feat: create AddNoteToBudgetCommand class

---
+++ libs/model/budgetting/notes/budget-notes/src/lib/domain/add-note.command.ts
@@ -0,0 +1,23 @@
+export interface BudgetNote {
+  id: string;
+  budgetId: string
+  content: string;
+  authorId: string;
+  createdAt: Date | string;
+}
+
+export class AddNoteToBudgetCommand {
+  readonly id: string;
+  readonly budgetId: string;
+  readonly content: string;
+  readonly authorId: string;
+  readonly createdAt: Date | string;
+
+  constructor(data: BudgetNote) {
+    this.id = data.id;
+    this.budgetId = data.budgetId;
+    this.content = data.content.trim();
+    this.authorId = data.authorId;
+    this.createdAt = data.createdAt;
+  }
+}
